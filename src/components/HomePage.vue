aaf0e401709380bfbd60ca78840fc9e25958088b
riyadahmed2809@gmail.com <92542706+riyad-ahmed@users.noreply.github.com>
2023-07-16 11:09:39 +0600

    new

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,133 +1,166 @@
 <template>
-  <section class="bg-blue-500 text-white py-16">
-    <div class="container mx-auto text-center">
-      <div class="relative">
-        <img
-          src="slider-image1.jpg"
-          alt="Slider Image 1"
-          class="mx-auto rounded-lg"
-        />
-        <img
-          src="slider-image2.jpg"
-          alt="Slider Image 2"
-          class="mx-auto rounded-lg absolute top-0 left-1/2 transform -translate-x-1/2 translate-y-1/4 opacity-0 scale-90 transition duration-500"
-        />
-        <img
-          src="slider-image3.jpg"
-          alt="Slider Image 3"
-          class="mx-auto rounded-lg absolute top-0 left-1/2 transform -translate-x-1/2 translate-y-1/4 opacity-0 scale-90 transition duration-500"
-        />
+  <div>
+    <section class="bg-blue-500 text-white py-16">
+      <div class="container mx-auto text-center">
+        <div class="glide">
+          <div class="glide__track" data-glide-el="track">
+            <ul class="glide__slides">
+              <!-- Slide 1 -->
+              <li
+                class="glide__slide w-full bg-center bg-cover"
+                v-for="item in sliderItems"
+                :key="item?.id"
+              >
+                <img :src="item?.image" alt="Slide 1" />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <h1 class="text-4xl font-bold mb-4">Welcome to Your App</h1>
+        <p class="text-lg mb-8">A powerful solution for all your needs</p>
+        <a
+          href="#menu"
+          class="bg-white text-blue-500 hover:bg-blue-100 text-lg px-6 py-3 rounded-full"
+          >Discover More</a
+        >
       </div>
-      <h1 class="text-4xl font-bold mb-4">Welcome to Your App</h1>
-      <p class="text-lg mb-8">A powerful solution for all your needs</p>
-      <a
-        href="#menu"
-        class="bg-white text-blue-500 hover:bg-blue-100 text-lg px-6 py-3 rounded-full"
-        >Discover More</a
-      >
-    </div>
-  </section>
+    </section>
 
-  <!-- About Section -->
-  <section class="py-16">
-    <div class="container mx-auto">
-      <div class="flex flex-col md:flex-row items-center">
-        <div class="md:w-1/2 md:pr-8">
-          <h2 class="text-3xl font-bold mb-4">About Us</h2>
-          <p class="mb-6">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            fringilla, diam eget dapibus aliquam, nisl quam condimentum justo.
-          </p>
-          <p class="mb-6">
-            Phasellus commodo, erat vel egestas rutrum, est lorem maximus orci,
-            eu tincidunt ex nisl id lectus.
-          </p>
-        </div>
-        <div class="md:w-1/2">
-          <img src="about-image.jpg" alt="About Us" class="rounded-lg" />
+    <!-- About Section -->
+    <section class="py-16">
+      <div class="container mx-auto">
+        <div class="flex flex-col md:flex-row items-center">
+          <div class="md:w-1/2 md:pr-8">
+            <h2 class="text-3xl font-bold mb-4">About Us</h2>
+            <p class="mb-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum fringilla, diam eget dapibus aliquam, nisl quam
+              condimentum justo.
+            </p>
+            <p class="mb-6">
+              Phasellus commodo, erat vel egestas rutrum, est lorem maximus
+              orci, eu tincidunt ex nisl id lectus.
+            </p>
+          </div>
+          <div class="md:w-1/2">
+            <img src="about-image.jpg" alt="About Us" class="rounded-lg" />
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
 
-  <!-- Menu Card Section -->
-  <section id="menu" class="bg-gray-100 py-16">
-    <div class="container mx-auto">
-      <h2 class="text-3xl font-bold mb-8 text-center">Our Menu</h2>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        <div class="bg-white shadow rounded-lg p-6">
-          <h3 class="text-xl font-bold mb-4">Menu Item 1</h3>
-          <p class="mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            fringilla, diam eget dapibus aliquam.
-          </p>
-          <a href="#" class="text-blue-500 hover:underline">Learn More</a>
-        </div>
-        <div class="bg-white shadow rounded-lg p-6">
-          <h3 class="text-xl font-bold mb-4">Menu Item 2</h3>
-          <p class="mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            fringilla, diam eget dapibus aliquam.
-          </p>
-          <a href="#" class="text-blue-500 hover:underline">Learn More</a>
-        </div>
-        <div class="bg-white shadow rounded-lg p-6">
-          <h3 class="text-xl font-bold mb-4">Menu Item 3</h3>
-          <p class="mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            fringilla, diam eget dapibus aliquam.
-          </p>
-          <a href="#" class="text-blue-500 hover:underline">Learn More</a>
+    <!-- Menu Card Section -->
+    <section id="menu" class="bg-gray-100 py-16">
+      <div class="container mx-auto">
+        <h2 class="text-3xl font-bold mb-8 text-center">Our Menu</h2>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div class="bg-white shadow rounded-lg p-6">
+            <h3 class="text-xl font-bold mb-4">Menu Item 1</h3>
+            <p class="mb-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum fringilla, diam eget dapibus aliquam.
+            </p>
+            <a href="#" class="text-blue-500 hover:underline">Learn More</a>
+          </div>
+          <div class="bg-white shadow rounded-lg p-6">
+            <h3 class="text-xl font-bold mb-4">Menu Item 2</h3>
+            <p class="mb-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum fringilla, diam eget dapibus aliquam.
+            </p>
+            <a href="#" class="text-blue-500 hover:underline">Learn More</a>
+          </div>
+          <div class="bg-white shadow rounded-lg p-6">
+            <h3 class="text-xl font-bold mb-4">Menu Item 3</h3>
+            <p class="mb-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum fringilla, diam eget dapibus aliquam.
+            </p>
+            <a href="#" class="text-blue-500 hover:underline">Learn More</a>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
 
-  <!-- Testimonial Section -->
-  <section class="py-16">
-    <div class="container mx-auto">
-      <h2 class="text-3xl font-bold mb-8 text-center">Testimonials</h2>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        <div class="bg-white shadow rounded-lg p-6">
-          <p class="mb-4">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            fringilla, diam eget dapibus aliquam."
-          </p>
-          <p class="font-bold">- John Doe</p>
-        </div>
-        <div class="bg-white shadow rounded-lg p-6">
-          <p class="mb-4">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            fringilla, diam eget dapibus aliquam."
-          </p>
-          <p class="font-bold">- Jane Smith</p>
-        </div>
-        <div class="bg-white shadow rounded-lg p-6">
-          <p class="mb-4">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            fringilla, diam eget dapibus aliquam."
-          </p>
-          <p class="font-bold">- David Johnson</p>
+    <!-- Testimonial Section -->
+    <section class="py-16">
+      <div class="container mx-auto">
+        <h2 class="text-3xl font-bold mb-8 text-center">Testimonials</h2>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div class="bg-white shadow rounded-lg p-6">
+            <p class="mb-4">
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum fringilla, diam eget dapibus aliquam."
+            </p>
+            <p class="font-bold">- John Doe</p>
+          </div>
+          <div class="bg-white shadow rounded-lg p-6">
+            <p class="mb-4">
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum fringilla, diam eget dapibus aliquam."
+            </p>
+            <p class="font-bold">- Jane Smith</p>
+          </div>
+          <div class="bg-white shadow rounded-lg p-6">
+            <p class="mb-4">
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum fringilla, diam eget dapibus aliquam."
+            </p>
+            <p class="font-bold">- David Johnson</p>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
 
-  <!-- Who Uses My App Section -->
-  <section class="bg-gray-100 py-16">
-    <div class="container mx-auto">
-      <h2 class="text-3xl font-bold mb-8 text-center">Who Uses Our App</h2>
-      <div class="flex justify-center">
-        <img src="client-logo1.png" alt="Client 1" class="h-16 mx-4" />
-        <img src="client-logo2.png" alt="Client 2" class="h-16 mx-4" />
-        <img src="client-logo3.png" alt="Client 3" class="h-16 mx-4" />
+    <section class="bg-gray-100 py-16">
+      <div class="container mx-auto">
+        <h2 class="text-3xl font-bold mb-8 text-center">Who Uses Our App</h2>
+        <div class="flex justify-center">
+          <img src="client-logo1.png" alt="Client 1" class="h-16 mx-4" />
+          <img src="client-logo2.png" alt="Client 2" class="h-16 mx-4" />
+          <img src="client-logo3.png" alt="Client 3" class="h-16 mx-4" />
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <script>
+import { faker } from "@faker-js/faker";
+import Glide from "@glidejs/glide";
 export default {
   name: "HomePage",
+  data() {
+    return {
+      sliderItems: [],
+    };
+  },
+  created() {
+    this.sliderItems = this.generateSliderItem(5);
+  },
+
+  methods: {
+    generateSliderItem(count) {
+      const sliderItems = [];
+
+      for (let i = 0; i < count; i++) {
+        const sliderItem = {
+          id: i + 1,
+          image: faker.image.abstract(2000, 400, true),
+        };
+        sliderItems.push(sliderItem);
+      }
+      console.log(sliderItems);
+      return sliderItems;
+    },
+  },
+  mounted() {
+    const glide = new Glide(".glide", {
+      type: "carousel",
+      autoplay: 5000,
+      hoverpause: true,
+    });
+    glide.mount();
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import router from './routes'
+import router from './routes';
+import '@glidejs/glide/dist/css/glide.core.min.css'
+
 
 createApp(App).use(router).mount('#app')
